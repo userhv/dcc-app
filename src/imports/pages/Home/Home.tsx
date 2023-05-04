54132a0a3d384b121131cf47466aec2a0a4e4d43
userhv <heliovictor05@hotmail.com>
2023-05-04 20:23:06 -0300

    develop: atualizando statusbar

--- src/imports/pages/Home/Home.tsx
+++ src/imports/pages/Home/Home.tsx
@@ -1,16 +1,17 @@
 //@ts-ignore
 import React  from 'react';
-import { View} from 'react-native';
+import { StatusBar, View} from 'react-native';
 import {homeStyle} from './homeStyles';
 import { HomeHeader } from './HomeHeader/HomeHeader';
 import { Divider } from 'react-native-paper';
-
+import { theme } from '../../paper/theme';
 
 export const Home = (props: any) => {
   const {user} = props;
 
   return (
     <View style={homeStyle.container}>
+      <StatusBar backgroundColor={theme.colors.branco}  barStyle={'dark-content'}/>
       <HomeHeader user={user} />
       <Divider style={homeStyle.divisor}/>
       <View style={homeStyle.blocoInterno}>
