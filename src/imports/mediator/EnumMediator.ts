a749a250fc4d90abcddc80cd48e6ed85e45fb475
userhv <heliovictor05@hotmail.com>
2023-05-25 17:07:34 -0300

    dcc-app-29: implementação da tela de noticias

--- src/imports/mediator/EnumMediator.ts
+++ src/imports/mediator/EnumMediator.ts
@@ -1,3 +1,5 @@
 export enum EnumMediator  {
-	NOTICIAS = 'noticias'
+	NOTICIAS = 'noticias',
+	EVENTOS = 'eventos',
+	PALESTRAS = 'palestras',
 };
---
+++ src/imports/modules/noticias/pages/style/noticiasSalvasStyle.tsx
@@ -0,0 +1,43 @@
+import {StyleSheet} from 'react-native';
+import {theme} from '../../../../paper/theme';
+
+export const noticiasSalvasRNStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 1,
+  },
+
+  containerTop: {
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.branco,
+    
+  },
+
+  descricao: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginLeft: 20,
+  },
+
+  boxIconeVazio: {
+    flex:1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  texto: {
+    flexWrap: 'wrap',
+    padding: 10,
+    textAlign: 'center'
+  },
+  
+  divisor: {
+    backgroundColor: theme.colors.cinzaMedio
+  },
+
+});
