00ce33d7b6c1db8575fe6383cf77a4755cce37bb
userhv <heliovictor05@hotmail.com>
2023-06-19 21:35:43 -0300

    Update de versão

--- src/imports/modules/menu/pages/subsecoes/Contatos.tsx
+++ src/imports/modules/menu/pages/subsecoes/Contatos.tsx
@@ -8,7 +8,7 @@ import { theme } from '../../../../paper/theme';
 import { useState } from 'react';
 import { Linking } from 'react-native';
 import qs from 'qs';
-import { ScrollView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
 
 interface IContatos {
     navigation: NativeStackNavigationProp<any>;
@@ -36,43 +36,44 @@ export const Contatos = (props: IContatos) => {
   return (
     <View style={subSecoesStyle.container}>
       <HeaderBar navigation={navigation} titulo='Fale conosco'/>
-      <ScrollView style={{flex: 1}}>
-        <View style={contatosStyle.boxAlerta}>
-          <View style={contatosStyle.descricao}>
-              <Text variant='labelLarge' style={{color: theme.colors.vermelhoVivo, paddingLeft: 5}} numberOfLines={4}> 
-              Ao enviar, você será redirecionado para o seu provedor de email padrão. </Text>
-          </View>
-        </View>
-          <View style={contatosStyle.form}>
-              <View style={contatosStyle.labelForm}>
-                <Text variant='labelLarge'> Escreva o seu feedback  </Text>
-              </View>
-            <TextInput
-                value={text}
-                onChangeText={text => setText(text)}
-                multiline
-                mode='outlined'
-                selectionColor={theme.colors.azul}
-                underlineColor="transparent"
-                activeUnderlineColor="transparent"
-                activeOutlineColor={theme.colors.azul}
-                numberOfLines={10}
-                textColor={theme.colors.preto}
-                contentStyle={{borderRadius: 8, padding: 5}}
-            />
-            <View style={{paddingTop: 10,flexDirection: 'row', justifyContent: 'flex-end'}}>
-              <Button
-                mode='contained'
-                icon='send'
-                disabled={text === ""}
-                style={{backgroundColor: text== "" ? theme.colors.cinza90: theme.colors.azul}}
-                onPress={async() => await enviarEmail()}>
-                Enviar
-              </Button>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <ScrollView>
+          <View style={contatosStyle.boxAlerta}>
+            <View style={contatosStyle.descricao}>
+                <Text variant='labelLarge' style={{color: theme.colors.vermelhoVivo, paddingLeft: 5}} numberOfLines={4}> 
+                Ao enviar, você será redirecionado para o seu provedor de email padrão. </Text>
             </View>
           </View>
-      </ScrollView>
-
+            <View style={contatosStyle.form}>
+                <View style={contatosStyle.labelForm}>
+                  <Text variant='labelLarge'> Escreva o seu feedback  </Text>
+                </View>
+              <TextInput
+                  value={text}
+                  onChangeText={text => setText(text)}
+                  multiline
+                  mode='outlined'
+                  selectionColor={theme.colors.azul}
+                  underlineColor="transparent"
+                  activeUnderlineColor="transparent"
+                  activeOutlineColor={theme.colors.azul}
+                  numberOfLines={10}
+                  textColor={theme.colors.preto}
+                  contentStyle={{borderRadius: 8, padding: 5}}
+              />
+              <View style={{paddingTop: 10,flexDirection: 'row', justifyContent: 'flex-end'}}>
+                <Button
+                  mode='contained'
+                  icon='send'
+                  disabled={text === ""}
+                  style={{backgroundColor: text== "" ? theme.colors.cinza90: theme.colors.azul}}
+                  onPress={async() => await enviarEmail()}>
+                  Enviar
+                </Button>
+              </View>
+            </View>
+        </ScrollView>
+      </GestureHandlerRootView>
     </View>
   );
 };
