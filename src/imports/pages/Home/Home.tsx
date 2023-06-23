90580c9adce5077a163b27ca86323ee26a16dea0
userhv <heliovictor05@hotmail.com>
2023-06-23 17:17:56 -0300

    develop: adição do banner de atualização

--- src/imports/pages/Home/Home.tsx
+++ src/imports/pages/Home/Home.tsx
@@ -1,15 +1,22 @@
 //@ts-ignore
 import React  from 'react';
-import { StatusBar, View} from 'react-native';
+import { Linking, StatusBar, View} from 'react-native';
 import {homeStyle} from './homeStyles';
 import { HomeHeader } from './HomeHeader/HomeHeader';
-import { Divider, Text } from 'react-native-paper';
+import { Banner, Divider, Text } from 'react-native-paper';
 import { theme } from '../../paper/theme';
 
 
 export const Home = (props: any) => {
   const {user} = props;
 
+  const [visible, setVisible] = React.useState(false);
+
+  const abrirPlayStore = async () => {
+    const url = 'https://play.google.com/store/apps/details?id=com.dcc.android';
+    const suportado = await Linking.canOpenURL(url);
+    if(suportado) return Linking.openURL(url);
+  }
 
   return (
     <View style={homeStyle.container}>
@@ -20,6 +27,25 @@ export const Home = (props: any) => {
         <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', backgroundColor: theme.colors.azulOpacoMenuOportunidades, padding: 5 }}>
 					<Text variant='titleSmall' style={{color: theme.colors.azul}}> PREVIEW </Text>
 				</View>
+      <Banner
+        visible={visible}
+        elevation={0}
+        style={{backgroundColor: theme.colors.vermelhoVivoOpaco, margin: 10, borderRadius: 10}}
+        actions={[{
+            label: 'Atualizar',
+            onPress: async () => {
+              await abrirPlayStore();
+              setVisible(false)},
+            buttonColor: theme.colors.vermelhoVivo,
+            textColor: theme.colors.branco,
+            mode: 'contained',
+            icon: 'update'
+        }
+        ]}>
+        <Text variant='labelLarge' style={{color: theme.colors.vermelhoVivo, paddingLeft: 5}} numberOfLines={4}> 
+            Uma nova versão do aplicativo está disponível.
+          </Text>
+      </Banner>
       </View>
     </View>
   );
